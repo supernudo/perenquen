202739f4e7af3e75971c9f0f4e282fc695a40a8e
supernudo <balinas@gmail.com>
2019-04-11 20:54:49 +0200

    [NEW] Telemetry module.

---
+++ Software/perenquen-board/telemetry.c
@@ -0,0 +1,69 @@
+/*
+ *  Copyright Robotics Association of Coslada, Eurobotics Engineering (2010)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Revision : $Id$
+ *
+ *  Javier Baliñas Santos <balinas@gmail.com>
+ */
+
+#include <stdint.h>
+
+#include <main.h>
+#include "telemetry.h"
+
+/* Telemetry data packet */
+struct tm_data tm;
+
+/* Initialize header of telemetry packet */
+void tm_data_init(void)
+{
+	tm.header[0] = TM_HEADER_BYTE_0;
+	tm.header[1] = TM_HEADER_BYTE_1;
+	tm.header[2] = TM_HEADER_BYTE_2;
+	tm.header[3] = TM_HEADER_BYTE_3;
+}
+
+/* Update telemetry packet and send it thru UART */
+void tm_data_send(void)
+{
+	void *pdata = (void*)&tm;
+	uint8_t data_size = sizeof(struct tm_block);
+  uint8_t i;
+
+	/* timetag */
+	tm.time_ms = time_get_us2()/1000;
+
+	/* angle CS */
+	tm.angle.consign = cs_get_consign(&mainboard.angle.cs);
+	tm.angle.fconsign = cs_get_filtered_consign(&mainboard.angle.cs);
+	tm.angle.error = cs_get_error(&mainboard.angle.cs);
+	tm.angle.ffeedback = cs_get_filtered_feedback(&mainboard.angle.cs);
+	tm.angle.out = cs_get_out(&mainboard.angle.cs);
+
+	/* distance CS */
+	tm.distance.consign = cs_get_consign(&mainboard.distance.cs);
+	tm.distance.fconsign = cs_get_filtered_consign(&mainboard.distance.cs);
+	tm.distance.error = cs_get_error(&mainboard.distance.cs);
+	tm.distance.ffeedback = cs_get_filtered_feedback(&mainboard.distance.cs);
+	tm.distance.out = cs_get_out(&mainboard.distance.cs);
+
+	/* TODO: add more data */
+
+	/* send data */
+	for(i=0; i<data_size; i++)
+		uart_send(0,*(char*)pdata++);
+}
---
+++ Software/perenquen-board/telemetry.h
@@ -0,0 +1,66 @@
+/*
+ *  Copyright Robotics Association of Coslada, Eurobotics Engineering (2010)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Revision : $Id$
+ *
+ *  Javier Baliñas Santos <balinas@gmail.com>
+ */
+
+#ifndef _TELEMETRY_H_
+#define _TELEMETRY_H_
+
+#include <stdint.h>
+
+#include <main.h>
+#include "telemetry.h"
+
+/* Telemetry data of each CS */
+struct tm_cs_data
+{
+	/* total 5 x 4 bytes = 20 bytes */
+	int32_t consign;
+	int32_t fconsign;
+	int32_t error;
+	int32_t ffeedback;
+	int32_t out;
+};
+
+/* Telemetry data packet */
+struct tm_data
+{
+	uint8_t header[4];						// 4 bytes
+	uint32_t time_ms;							// 4 bytes
+	struct tm_cs_data angle;			// 20 bytes
+	struct tm_cs_data distance;		// 20 bytes
+																// 48 bytes total
+
+	// wall sensors: 4 x 2 = 8 bytes
+	// gyro: 								 2 bytes
+	// battery: 						 1 byte
+	// encoders:		 2 x 4 = 8 bytes
+	// x, y, a:			 3 x 2 = 6 bytes
+	// i, j:				 2 x 1 = 2 bytes
+	// slot_info:						 1 byte
+	// total:							  74 bytes
+};
+
+
+/* Initialize header of telemetry packet */
+void tm_data_init(void);
+
+/* Update telemetry packet and send it thru UART */
+void tm_data_send(void);
